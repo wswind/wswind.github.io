68f3693ed4b9aae198b415be97eb28a7fdeb90e7
Vincent Wang <ws_dev@163.com>
2023-01-08 23:04:06 +0800

    add publish bash script

---
+++ gh-pages.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+hugo
+cd public
+git init -b gh-pages
+git remote add origin https://github.com/wswind/wswind.github.io
+git add .
+git commit -m "update github pages"
+git push origin gh-pages -f
+rm -rf .git
